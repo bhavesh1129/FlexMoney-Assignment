debd98dc69f1e15b8718b24dde931ae34ee35f10
bhavesh1129 <bhavesh0336.cse19@chitkara.edu.in>
2022-12-10 12:27:28 +0530

    first commit

---
+++ frontend/src/Components/Completion.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Completion.css'
+import imgg from './../1.png'
+function Completion() {
+
+    //After completion of payment just return to homepage
+    const ReturnToHome = () => {
+        window.location.reload()
+    }
+    return (
+        <div className="completionContainer">
+            <img src={imgg} />
+            <h>Payment Complete</h>
+            <div className="details">
+                <h>Name :{String(localStorage.getItem("name"))} </h>
+                <h>Age : {String(localStorage.getItem("age"))} years </h>
+                <h>Start Date : {String(localStorage.getItem("startDate"))} </h>
+                <h>Batch : {String(localStorage.getItem("batchNumber"))} </h>
+                <h>Fees Paid : 500 Rs. </h>
+            </div>
+            <button type="button"
+                onClick={ReturnToHome}
+            >
+                Return to home <i class="fas fa-home"></i></button>
+        </div>
+    )
+}
+
+export default Completion
---
+++ frontend/src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+function Header() {
+    return (
+        <div className="yogaHeader">
+            <h2>Admission Form</h2>
+            <h4>Yoga Classes</h4>
+        </div>
+    )
+}
+
+export default Header
